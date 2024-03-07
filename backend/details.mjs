d2e9a3bad5a6ba1267dcd6f76c2b5eb1641a32f2
Kothakota Deepika <kothakota.deepika_ext@mediakind.com>
2024-03-07 14:20:59 +0530

    integrated operatorportal api

---
+++ backend/details.mjs
@@ -0,0 +1,12 @@
+// import express from 'express';
+// import fileUpload from 'express-fileupload';
+// const router= express.Router();
+// import expressFileUpload from 'express-fileupload';
+
+// router.use(fileUpload())
+// router.post('/',(req,res) => {
+//     console.log(req.files);
+//     res.status(200).json({message:'ok'});
+// })
+
+// export default router;
